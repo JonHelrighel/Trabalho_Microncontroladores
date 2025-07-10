3ca19ea33d2d39d1a32ee18b325f883d2673c4d4
Dawnieljs <daniel.j11@aluno.ifsc.edu.br>
2025-07-10 16:56:12 -0300

    Realiza a transmissão e recepção de dados

--- Trabalho_PWM/main.c
+++ Trabalho_PWM/main.c
@@ -7,8 +7,8 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define PPR 1       // 1 pulso por revolu��o (ajustado ao seu disco)
-#define FILTER_N 5  // N�mero de amostras na m�dia m�vel
+#define PPR 1       // 1 pulso por revolucao (ajustado ao seu disco)
+#define FILTER_N 5  // Numero de amostras na media movel
 
 // --- UART 9600 8N1 ---
 void UART_init(void) {
@@ -18,4 +18,19 @@ void UART_init(void) {
 	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
 	UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
 }
-//teste pra ver se esta funcionando pro daniel
+
+void UART_send_char(char c) {
+    while (!(UCSR0A & (1<<UDRE0)));
+    UDR0 = c;
+}
+
+void UART_send_string(const char *s) {
+    while (*s) UART_send_char(*s++);
+}
+
+char UART_recv_char(void) {
+    while (!(UCSR0A & (1<<RXC0)));
+    return UDR0;
+}
+
+
